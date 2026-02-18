08f432d9002fd59657711f51987026636b504c1e
David Hordiienko <shiraz0david@gmail.com>
2026-02-18 15:01:16 +0200

    improve: parseUtmParams

--- src/utils/parse-utm-params.test.ts
+++ src/utils/parse-utm-params.test.ts
@@ -5,14 +5,14 @@ test("utm_source", () => {
   const urlParams = new URLSearchParams("?utm_source=source");
 
   expect(parseUtmParams(urlParams)).toEqual({
-    utm_source: "source",
+    utm_source: "source"
   });
 });
 test("utm_medium", () => {
   const urlParams = new URLSearchParams("?utm_medium=medium");
 
   expect(parseUtmParams(urlParams)).toEqual({
-    utm_medium: "medium",
+    utm_medium: "medium"
   });
 });
 
@@ -20,7 +20,7 @@ test("utm_campaign", () => {
   const urlParams = new URLSearchParams("?utm_campaign=campaign");
 
   expect(parseUtmParams(urlParams)).toEqual({
-    utm_campaign: "campaign",
+    utm_campaign: "campaign"
   });
 });
 
@@ -28,7 +28,7 @@ test("utm_term", () => {
   const urlParams = new URLSearchParams("?utm_term=term");
 
   expect(parseUtmParams(urlParams)).toEqual({
-    utm_term: "term",
+    utm_term: "term"
   });
 });
 
@@ -36,30 +36,48 @@ test("utm_content", () => {
   const urlParams = new URLSearchParams("?utm_content=content");
 
   expect(parseUtmParams(urlParams)).toEqual({
-    utm_content: "content",
+    utm_content: "content"
   });
 });
 
 test("Full list of parameters", () => {
-  const urlParams = new URLSearchParams(
-    "?utm_source=source&utm_medium=medium&utm_campaign=campaign&utm_term=term&utm_content=content"
-  );
+  const urlParams = new URLSearchParams("?utm_source=source&utm_medium=medium&utm_campaign=campaign&utm_term=term&utm_content=content");
 
   expect(parseUtmParams(urlParams)).toEqual({
     utm_source: "source",
     utm_medium: "medium",
     utm_campaign: "campaign",
     utm_term: "term",
-    utm_content: "content",
+    utm_content: "content"
   });
 });
 
 test("Parameter duplicate", () => {
-  const urlParams = new URLSearchParams(
-    "?utm_source=source&utm_source=source2"
-  );
+  const urlParams = new URLSearchParams("?utm_source=source&utm_source=source2");
 
   expect(parseUtmParams(urlParams)).toEqual({
-    utm_source: "source",
+    utm_source: "source"
+  });
+});
+
+test("UTM trim test", () => {
+  const urlParams = new URLSearchParams("?utm_source=&utm_source=source2");
+
+  expect(parseUtmParams(urlParams)).toEqual({});
+});
+
+test("UTM trim test 2", () => {
+  const urlParams = new URLSearchParams("?utm_source= source &utm_medium=    ");
+
+  expect(parseUtmParams(urlParams)).toEqual({
+    utm_source: "source"
+  });
+});
+
+test("UTM trim test 3", () => {
+  const urlParams = new URLSearchParams("?utm_source=%20source");
+
+  expect(parseUtmParams(urlParams)).toEqual({
+    utm_source: "source"
   });
 });
--- src/utils/parse-utm-params.ts
+++ src/utils/parse-utm-params.ts
@@ -1,13 +1,7 @@
 export function parseUtmParams(urlSearchParams: URLSearchParams) {
   const utm: Record<string, string> = {};
-  [
-    "utm_campaign",
-    "utm_source",
-    "utm_medium",
-    "utm_term",
-    "utm_content",
-  ].forEach((key) => {
-    const value = urlSearchParams.get(key);
+  ["utm_campaign", "utm_source", "utm_medium", "utm_term", "utm_content"].forEach((key) => {
+    const value = urlSearchParams.get(key)?.trim();
     if (value) {
       utm[key] = value;
     }
